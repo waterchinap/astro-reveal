cb633f631c8ef4f1d3c039dfee8efe4a36fa8661
waterchinap <waterchinap@qq.com>
2026-07-05 15:14:53 +0800

    daily update

---
+++ backend/csi_indu.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.21.1"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import polars as pl
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
