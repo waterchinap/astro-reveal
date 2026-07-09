679ba6e62092d646aece3566b0ca2f7f834d8dd4
waterchinap <waterchinap@qq.com>
2026-07-09 09:44:38 +0800

    daily update

---
+++ backend/json_fetcher.py
@@ -0,0 +1,41 @@
+import curl_cffi
+import json
+from pathlib import Path
+from dataclasses import dataclass
+import typer
+
+
+from loguru import logger
+
+app = typer.Typer()
+
+
+@dataclass
+class JsonInfo:
+    name: str
+    url: str
+
+
+url_map = {
+    "index_perf": "https://www.csindex.com.cn/csindex-home/data-service/indexPerformance"
+}
+url = "https://www.csindex.com.cn/csindex-home/data-service/sector-change"
+
+
+def get_a_json(data: JsonInfo):
+    r = curl_cffi.get(data.url, impersonate="chrome")
+    fn = Path(__file__).parent.parent / "src" / "assets" / (data.name + ".json")
+    with open(fn, "w") as f:
+        json.dump(r.json(), f, ensure_ascii=False, indent=4)
+
+    logger.info(r.json())
+
+
+@app.command()
+def main(name: str):
+    item = JsonInfo(name=name, url=url_map[name])
+    get_a_json(item)
+
+
+if __name__ == "__main__":
+    app()
